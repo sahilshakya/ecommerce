e682b852843b1049db1a9a500eebff3c0c0c2a7d
sahil shakya <sahilshakya42@gmail.com>
2023-05-16 17:48:05 +0545

    feat: added navabar/sidebar,login, logout

---
+++ ecommerce-admin-portal/component/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "./SideBar";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Layout;
--- ecommerce-admin-portal/pages/index.js
+++ ecommerce-admin-portal/pages/index.js
@@ -1,6 +1,20 @@
-export default function Component() {
-  return (
-    <>Ecommerce Admin</>
-  )
-}
+import Layout from "@/component/Layout";
+import { useSession, signIn, signOut } from "next-auth/react";
 
+export default function Component() {
+  const { data: session } = useSession();
+
+  return (
+    <Layout>
+      <div className="text-blue-700 flex  justify-center">
+        <h2>
+          Hello, <b>{session?.user?.email}</b>
+        </h2>
+        <div>
+          <img src={session?.user?.image}></img>
+          <span>{session?.user?.email}</span>
+        </div>
+      </div>
+    </Layout>
+  );
+}
