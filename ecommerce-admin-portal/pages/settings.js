2c3ba905cbff9e0b1da2f7d6324e4b826e617361
sahil shakya <sahilshakya42@gmail.com>
2023-05-22 00:08:09 +0545

    fix: dashboard and setting page content added

--- ecommerce-admin-portal/pages/settings.js
+++ ecommerce-admin-portal/pages/settings.js
@@ -3,10 +3,25 @@ import React from "react";
 
 const settings = () => {
   return (
-    <div className="flex">
-      <Layout></Layout>
-      settings
-    </div>
+    <Layout>
+      <h2>Edit Profile</h2>
+      <form>
+        <label>Your Name</label>
+        <input type="text" name="name" />
+        <label>Your Email</label>
+        <input type="email" name="email" />
+        <label>Change Passwrd</label>
+        <input type="password" name="password" />
+        <label>Confirm Change Passwrd</label>
+        <input type="password" name="passwordConfirmation" />
+        <button
+          type="submit"
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >
+          Save
+        </button>
+      </form>
+    </Layout>
   );
 };
 
